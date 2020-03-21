644f30c50291406d1a5c310cb6a2156b995a23d4
william-schlegel <william.schlegel@gmail.com>
2020-03-21 10:39:00 +0100

    annonces tronquées sur 3 lignes + ellipsis + click

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
+(function () {
+	'use strict';
+	window.addEventListener('load', function () {
+		$('.text-annonce').on('click', function (event) {
+			if ($(this).css('overflow') === 'hidden') {
+				$(this).css({ overflow: 'visible', display: 'block' });
+			} else {
+				$(this).css({ overflow: 'hidden', display: '-webkit-box' });
+			}
+		});
+	});
+})();
+
 $(document).ready(function () {
 	var annonce;
 	var request;
