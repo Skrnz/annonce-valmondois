5f48fc67b485040ac0b62df82aa199bd4bca7eaa
william-schlegel <william.schlegel@gmail.com>
2020-04-01 23:34:01 +0200

    uploads

--- routes/api/download.js
+++ routes/api/download.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = async (req, res) => {
 	const annonce = await keystone.list('Annonce').model.findByIdAndUpdate(req.params.id, { $inc: { nbClick: 1 } });
 	if (annonce) {
-		const path = keystone.expandPath(annonce.fichier.path) + '/' + unescape(annonce.fichier.originalname);
+		const path = keystone.expandPath(annonce.fichier.path) + '/uploads/' + unescape(annonce.fichier.originalname);
 		return fs.access(path, fs.constants.F_OK, (err) => {
 			if (err) {
 				return res.status(500).json({ error: 'erreur, le fichier n\'existe pas' });
